aae29331195a52556c413689a7fa8be5e2bbc01e
Eric Evans <evans2designs@gmail.com>
2020-11-03 08:44:15 -0700

    Refactor code to functions file

--- coursework/notes-app/notes-app.js
+++ coursework/notes-app/notes-app.js
@@ -1,33 +1,12 @@
 // DOM - Document Object Model
 
-let notes = []
+const notes = getSavedNotes()
 
 const filters = {
     searchText: ''
 }
 
-// Check for existing notes data
-const notesJSON = localStorage.getItem('notes')
-if (notesJSON !== null){
-    notes = JSON.parse(notesJSON)
-}
 
-const renderNotes = function(notes, filters) {
-    const filteredNotes = notes.filter(function(note){
-        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
-    })
-
-    document.querySelector('#notes').innerHTML = ''
-    filteredNotes.forEach(function(note){
-        const noteEl = document.createElement('p')
-        if (note.title.length > 0){
-            noteEl.textContent = note.title
-        } else {
-            noteEl.textContent = 'Unamed Note'
-        }
-        document.querySelector('#notes').appendChild(noteEl)
-    })
-}
 
 renderNotes(notes, filters)
 
