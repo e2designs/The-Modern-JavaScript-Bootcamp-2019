408ab108547bb52711d4f4b482b6242fa2217d98
Eric Evans <evans2designs@gmail.com>
2020-11-03 17:09:31 -0700

    Refactor

--- coursework/todo-app/todo-app.js
+++ coursework/todo-app/todo-app.js
@@ -1,47 +1,8 @@
-// 1. Delete Dummy Data
-// 2. Read and Parse the data when the app starts up.
-// 3. Stringify and write the data when new data is added.
-
-let todos = []
+let todos = getSavedTodos()
 
 const filters = {
     searchText: '',
     hideCompleted: false
-}
-
-const todoJSON = localStorage.getItem('todos')
-if (todoJSON !== null){
-    todos = JSON.parse(todoJSON)
-}
-
-const notComplete = function(todos){
-    return todos.filter(function(todo){
-        return !todo.completed
-    })
-}
-
-const renderTodos = function(todos, filters){
-    const filteredTodos = todos.filter(function(todo){
-        matchedTodo = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
-        hideCompleted = !filters.hideCompleted || !todo.completed
-        
-        return matchedTodo && hideCompleted
-    })
-    const numToDo = notComplete(filteredTodos).length
-
-    document.querySelector('#todos').innerHTML = ''
-    const notCompEl = document.createElement('h2')
-    notCompEl.textContent = `You have ${numToDo} todos incomplete.`
-    document.querySelector('#todos').appendChild(notCompEl)
-    filteredTodos.forEach(function(todo){
-        const todoElement = document.createElement('p')
-        if (todo.text.length > 0){
-            todoElement.textContent = todo.text
-        } else {
-            todoElement.textContent = 'Undefined Todo'
-        }
-        document.querySelector('#todos').appendChild(todoElement)
-    })
 }
 
 renderTodos(todos, filters)
@@ -56,14 +17,15 @@ document.querySelector('#add-todo').addEventListener('submit', function(e){
     e.preventDefault()
     todos.push({
         text: e.target.elements.newToDo.value, 
-        complete: false
+        completed: false
     })
-    localStorage.setItem('todos', JSON.stringify(todos))
+    saveTodos(todos)
     renderTodos(todos, filters)   
     e.target.elements.newToDo.value = ''
 })
 
 document.querySelector('#hide-completed').addEventListener('change', function(e){
     filters.hideCompleted = e.target.checked
+    console.log(`Hiding Completed: ${JSON.stringify(filters)}`)
     renderTodos(todos, filters)
 })
---
+++ coursework/todo-app/todo-functions.js
@@ -0,0 +1,52 @@
+// Fetch existing todos from localStorage
+const getSavedTodos = function(){
+    const todosJSON = localStorage.getItem('todos')
+    if (todosJSON !== null){
+        return JSON.parse(todosJSON)
+    } else {
+        return []
+    }
+}
+
+// Save todos to localStorage
+const saveTodos = function(todos){
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+// Render application todos based on filters
+const renderTodos = function(todos, filters){
+    const filteredTodos = todos.filter(function(todo){
+        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        const hideCompleted = !filters.hideCompleted || !todo.completed
+        console.log(`Hide Completed eval: ${hideCompleted}`) 
+        return searchTextMatch && hideCompleted
+    })
+
+    const incompleteTodos = filteredTodos.filter(function (todo){
+        return !todo.completed
+    })
+    console.log(`INCOMPLETE: ${JSON.stringify(incompleteTodos)}`)
+    document.querySelector('#todos').innerHTML = ''
+    document.querySelector('#todos').appendChild(generateTodoSum(incompleteTodos))
+    filteredTodos.forEach(function(todo){
+        document.querySelector('#todos').appendChild(generateTodoDOM(todo))
+    })
+}
+
+// Generate DOM structure for a todo
+const generateTodoDOM = function(todo){
+    const todoElement = document.createElement('p')
+    if (todo.text.length > 0){
+        todoElement.textContent = todo.text
+    } else {
+        todoElement.textContent = 'Undefined Todo'
+    }
+    return todoElement
+}
+
+// Generate DOM structure for summary
+const generateTodoSum = function (incompleteTodos){
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos incomplete.`
+    return summary
+}
