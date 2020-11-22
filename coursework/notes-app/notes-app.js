756f43b0d471d59ed2ee246ad25a331629d719ca
Eric Evans <evans2designs@gmail.com>
2020-11-22 10:10:35 -0700

    Reactive pages

--- coursework/notes-app/notes-app.js
+++ coursework/notes-app/notes-app.js
@@ -1,7 +1,7 @@
 // DOM - Document Object Model
 console.log(uuidv4())
 
-const notes = getSavedNotes()
+let notes = getSavedNotes()
 
 const filters = {
     searchText: ''
@@ -28,4 +28,11 @@ document.querySelector('#search-text').addEventListener('input', function(e){
 
 document.querySelector('#filter-by').addEventListener('change', function(e){
     console.log(e.target.value)
+})
+
+window.addEventListener('storage', function(e){
+    if (e.key === 'notes') {
+        notes = JSON.parse(e.newValue)
+        renderNotes(notes, filters)
+    }
 })
--- coursework/notes-app/notes-edit.js
+++ coursework/notes-app/notes-edit.js
@@ -3,8 +3,8 @@ const bodyElement = document.querySelector('#note-body')
 const removeElement = document.querySelector('#remove-note')
 const submitElement = document.querySelector('#submit-note')
 const noteId = location.hash.substring(1)
-const notes = getSavedNotes()
-const note = notes.find(function (note){
+let notes = getSavedNotes()
+let note = notes.find(function (note){
     return note.id === noteId
 })
 
@@ -34,4 +34,20 @@ removeElement.addEventListener('click', function(e) {
 submitElement.addEventListener('click', function(e){
     saveNotes(notes)
     location.assign('/index.html')
+})
+
+window.addEventListener('storage', function(e){
+    if (e.key === 'notes'){
+        notes = JSON.parse(e.newValue)
+        note = notes.find(function (note) {
+            return note.id === noteId
+        })
+
+        if (note === undefined) {
+            location.assign('/index.html')
+        }
+
+        titleElement.value = note.title
+        bodyElement.value = note.body
+    }
 })
