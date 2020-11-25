ea8761da41f788cb67505532ece54a3475c7a109
Eric Evans <evans2designs@gmail.com>
2020-11-24 17:58:25 -0700

    Updated with ternary operator

--- coursework/notes-app/notes-functions.js
+++ coursework/notes-app/notes-functions.js
@@ -5,11 +5,7 @@
 // Read existing notes data from localStorage
 const getSavedNotes = () => {
     const notesJSON = localStorage.getItem('notes')
-    if (notesJSON !== null){
-        return JSON.parse(notesJSON)
-    } else {
-        return []
-    }
+    return notesJSON !== null ? JSON.parse(notesJSON) : []
 }
 
 // Save the notes to localStorage
