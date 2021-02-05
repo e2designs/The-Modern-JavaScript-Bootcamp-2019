3b71a9f93811f8188f2aaa38b29871aa413be499
Eric Evans <evans2designs@gmail.com>
2021-02-05 06:14:33 -0700

    Conversion to class syntax

--- coursework/hangman/hangman.js
+++ coursework/hangman/hangman.js
@@ -1,67 +1,67 @@
 
 // Function for tracking the word, guessedLetters and remainingGuesses
-const Hangman = function (word, remainingGuesses) {
-    this.word = word.toLowerCase().split('')
-    this.guessedLetters = []
-    this.remainingGuesses = remainingGuesses
-    this.status = 'playing'
-}
+class Hangman {
+    constructor(word, remainingGuesses) {
+        this.word = word.toLowerCase().split('')
+        this.guessedLetters = []
+        this.remainingGuesses = remainingGuesses
+        this.status = 'playing'
+    }
+    getPuzzle () {
+        let puzzle = ''
 
-// Get Puzzle prototype - Returns the curent puzzle state (solved or unsolved letters)
-Hangman.prototype.getPuzzle = function () {
-    let puzzle = ''
+        this.word.forEach((letter) => {
+            if (this.guessedLetters.includes(letter) || letter === ' ') {
+                puzzle += letter
+            } else {
+                puzzle += '*'
+            }
+        })
 
-    this.word.forEach((letter) => {
-        if (this.guessedLetters.includes(letter) || letter === ' ') {
-            puzzle += letter
+        return puzzle
+    }
+
+    getStatus () {
+        if (this.status === 'playing') {
+            return `Remaining Guesses: ${this.remainingGuesses}`
+        } else if (this.status === 'finished') {
+            return `Great work! You guessed the word correctly!`
+        } else if (this.status === 'failed') {
+            return `Nice try! The word was "${this.word.join('')}".`
         } else {
-            puzzle += '*'
+            return `ERROR: ${this.status}`
         }
-    })
+    }
 
-    return puzzle
-}
+    makeGuess (guess) {
+        guess = guess.toLowerCase()
+        if (this.status != 'playing') {
+            return
+        }
+        const isUnique = !this.guessedLetters.includes(guess)
+        const isBadGuess = !this.word.includes(guess)
+        if (isUnique) {
+            this.guessedLetters.push(guess)
+        } else {
+            console.log(`Guess: '${guess}' has already been used`)
+        }
 
-Hangman.prototype.getStatus = function () {
-    if (this.status === 'playing') {
-        return `Remaining Guesses: ${this.remainingGuesses}`
-    } else if (this.status === 'finished' ) {
-        return `Great work! You guessed the word correctly!`
-    } else if (this.status === 'failed') {
-        return `Nice try! The word was "${this.word.join('')}".`
-    } else {
-        return `ERROR: ${this.status}`
+        if (isUnique && isBadGuess) {
+            this.remainingGuesses--
+        }
+        this.updateStatus()
     }
-}
 
-// Process guess from user
-Hangman.prototype.makeGuess = function (guess) {
-    guess = guess.toLowerCase()
-    if (this.status != 'playing') {
-        return
+    updateStatus () {
+        const finished = this.getPuzzle() === this.word.join('')
+        const failed = this.remainingGuesses < 1
+        if (finished && !failed) {
+            this.status = 'finished'
+        } else if (failed && !finished) {
+            this.status = 'failed'
+        }
     }
-    const isUnique = !this.guessedLetters.includes(guess)
-    const isBadGuess = !this.word.includes(guess)
-    if (isUnique) {
-        this.guessedLetters.push(guess)
-    } else {
-        console.log(`Guess: '${guess}' has already been used`)
-    }
-    
-    if (isUnique && isBadGuess) {
-        this.remainingGuesses--
-    }
-    this.updateStatus()
-}
 
-Hangman.prototype.updateStatus = function() {
-    const finished = this.getPuzzle() === this.word.join('')
-    const failed = this.remainingGuesses < 1
-    if (finished && !failed){
-        this.status = 'finished'
-    } else if (failed && !finished) {
-        this.status = 'failed'
-    }
 }
 
 const renderPuzzle = (game) => {
--- coursework/oop/person.js
+++ coursework/oop/person.js
@@ -1,27 +1,34 @@
 // Constructor function
+class Person {
+    constructor(firstName, lastName, age, likes = []) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.likes = likes 
+    }
 
-// Prototypal Inheritance
-const Person = function (firstName, lastName, age, likes = []) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-    this.likes = likes
+    getBio() {
+        let bio = `${this.firstName} is ${this.age}.`
+
+        this.likes.forEach((like) => {
+            bio += ` ${this.firstName} likes ${like}.`
+        })
+        return bio 
+    }
+
+    setName(fullName) {
+        const names = fullName.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[1] 
+    }
 }
 
-Person.prototype.getBio = function () {
-    let bio = `${this.firstName} is ${this.age}.`
+const myPerson = new Person('Eric', 'Evans', 46, ['Teaching'])
+console.log(myPerson)
+console.log(myPerson.getBio())
+console.log(myPerson.setName('Julie Park'))
+console.log(myPerson)
 
-    this.likes.forEach((like) => {
-        bio += ` ${this.firstName} likes ${like}.`
-    })
-    return bio
-}
-
-Person.prototype.setName = function (fullName) {
-    const names = fullName.split(' ')
-    this.firstName = names[0]
-    this.lastName = names[1] 
-}
 
 const me = new Person('Eric', 'Evans', '45', ['Python', 'Woodworking'])
 
