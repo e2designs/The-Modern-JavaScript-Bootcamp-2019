dc3af6425e0e46009b35c51b83744a852684516e
Eric Evans <evans2designs@gmail.com>
2021-02-09 06:02:39 -0700

    Setter with previous location storage

--- coursework/oop/get-set.js
+++ coursework/oop/get-set.js
@@ -1,6 +1,8 @@
 const data = {
+    locations: [],
     set location(value) {
         this._location = value.trim()
+        this.locations.push(value.trim())
     },
     get location() {
         return this._location
@@ -11,3 +13,8 @@ const data = {
 data.location = '  Philadelphia     '
 console.log(data)
 console.log(data.location)
+data.location = ' New York'
+console.log(data)
+console.log(data.locations)
+console.log(data.location)
+
